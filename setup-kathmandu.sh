629cc29afed436d225e2a6b5c5fab692bad8853a
Filip Krikava <krikava@gmail.com>
2019-02-14 18:16:54 +0100

    Removed setup files that are no longer needed

--- setup-kathmandu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export GLOBAL_IVY_DIR=~/.ivy2
-export GLOBAL_SBT_BOOT_DIR=~/.sbt/boot
-export N_JOBS=1
-export TIMEOUT=60m
-export SBT_MEM=8196
-export MAX_MEM=8196
--- setup-prl4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export GLOBAL_IVY_DIR=/mnt/nvme1/scala/ivy
-export GLOBAL_SBT_BOOT_DIR=/mnt/nvme1/scala/sbt-boot
-export N_JOBS=12
