9a1ead4328378678e79092b890176baace16198a
Filip Krikava <krikava@gmail.com>
2019-03-10 18:04:16 +0100

    Added quick summary to be run after implicits

---
+++ scripts/quick-summary.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(tidyverse))
+library(fs)
+library(knitr)
+
+status <- read_csv("implicits-status.csv")
+stats <- read_csv("implicits-stats.csv")
+
+cat("Implicits summary:\n")
+status %>% count(exit_code) %>% kable()
+
+cat("\nStats:\n")
+stats %>% 
+  mutate(failure=if_else(is.na(failure), 0, 1)) %>% 
+  summarise_at(vars(-project_id), sum) %>% 
+  gather() %>% 
+  kable()
