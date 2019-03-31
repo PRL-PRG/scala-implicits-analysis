9a62a6fc265990a59251bb774374d5466dab412f
Filip Krikava <krikava@gmail.com>
2019-03-31 12:55:16 +0200

    Prepared a test for type classes

--- libs/tools/src/test/scala/cz/cvut/fit/prl/scala/implicits/tools/ImplicitConversionSuite.scala
+++ libs/tools/src/test/scala/cz/cvut/fit/prl/scala/implicits/tools/ImplicitConversionSuite.scala
@@ -14,6 +14,41 @@ class ImplicitConversionSuite
   import ModelDSL._
   import ImplicitConversionExporter.export
 
+  project("conversion export of json example",
+    """
+      | package p
+      | object o {
+      |   trait Jsonable[T] {
+      |     def toJson(x: T): String
+      |   }
+      |
+      |   implicit val intJsonable = new Jsonable[Int] {
+      |     def toJson(x: Int): String = x.toString
+      |   }
+      |
+      |   implicit def seqJsonable[T: Jsonable]: Jsonable[Seq[T]] = new Jsonable[Seq[T]] {
+      |     def toJson(xs: Seq[T]): String = {
+      |       xs.map(_.toJson).mkString(",")
+      |     }
+      |   }
+      |
+      |   implicit class XtensionJson[T: Jsonable](x: T) {
+      |     def toJson: String = implicitly[Jsonable[T]].toJson(x)
+      |   }
+      |
+      |   Seq(1,2,3).toJson
+      | }
+    """.stripMargin) { implicit project =>
+      val conversion = export(project)
+      val declarations = project.implicitDeclarations.toList
+      val callSites = project.implicitCallSites.toList
+
+      println()
+  }
+
+
+
+
   index(
     "simple type parents",
     """
