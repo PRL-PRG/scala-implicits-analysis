d90cc68fd2209ea53072e20875ad0681df207459
Filip Krikava <krikava@gmail.com>
2019-03-31 12:54:10 +0200

    Added some progress logging for the composite exporter

--- libs/tools/src/main/scala/cz/cvut/fit/prl/scala/implicits/tools/CompositeMultiProjectExporter.scala
+++ libs/tools/src/main/scala/cz/cvut/fit/prl/scala/implicits/tools/CompositeMultiProjectExporter.scala
@@ -1,14 +1,24 @@
 package cz.cvut.fit.prl.scala.implicits.tools
+import com.typesafe.scalalogging.Logger
+import cz.cvut.fit.prl.scala.implicits.model.Util.timedTask
 import cz.cvut.fit.prl.scala.implicits.model.Project
+import org.slf4j.LoggerFactory
 
 import scala.util.Try
 
 class CompositeMultiProjectExporter(exporters: Seq[MultiProjectExporter]) extends MultiProjectExporter {
+  implicit val logger: Logger = Logger(LoggerFactory.getLogger(getClass.getName))
+  private var exported = 0
+
   override def export(project: Project): Unit = {
-    exporters.foreach(x => Try(x.export(project)))
+    exported = exported + 1
+    timedTask(s"Exporting project ($exported) ${project.projectId}") {
+      exporters.foreach(x => Try(x.export(project)))
+    }
   }
 
   override def close(): Unit = {
+    exported = 0
     exporters.map(x => Try(x.close())).foreach(_.get)
   }
 }
