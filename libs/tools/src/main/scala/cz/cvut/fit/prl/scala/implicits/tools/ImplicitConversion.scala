d970610e2f0d31a5118f6ac8472dfbb8fe413422
Filip Krikava <krikava@gmail.com>
2019-08-11 22:56:47 +0200

    Add missing group_id, artifact_id and version

--- libs/tools/src/main/scala/cz/cvut/fit/prl/scala/implicits/tools/ImplicitConversion.scala
+++ libs/tools/src/main/scala/cz/cvut/fit/prl/scala/implicits/tools/ImplicitConversion.scala
@@ -18,6 +18,9 @@ case class ImplicitConversion(
 
   def projectId: String = module.projectId
   def moduleId: String = module.moduleId
+  def groupId: String = module.groupId
+  def artifactId: String = module.artifactId
+  def version: String = module.version
   def declarationId: String = declaration.declarationId
   def isLocal: Boolean = declaration.isProjectLocal || declaration.isImplicitClassCompanionDef(module)
   def fromType: String = from.resolveFullType.asCode
@@ -47,6 +50,9 @@ object ImplicitConversion {
         Seq(
           "project_id",
           "module_id",
+          "group_id",
+          "artifact_id",
+          "version",
           "declaration_id",
           "is_local",
           "from",
@@ -74,6 +80,9 @@ object ImplicitConversion {
           Seq[Value](
             d.projectId,
             d.moduleId,
+            d.groupId,
+            d.artifactId,
+            d.version,
             d.declarationId,
             d.isLocal,
             d.fromType,
